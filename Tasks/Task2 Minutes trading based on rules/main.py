a9f350571f9b137384cfa30f6cb25ab8bcf879f5
Zuo <kuzaowuwei@kuzaowuweideMacBook-Pro-2.local>
2022-03-01 15:14:58 +0800

    Transaction cost

--- Tasks/Task2 Minutes trading based on rules/main.py
+++ Tasks/Task2 Minutes trading based on rules/main.py
@@ -46,18 +46,41 @@ Prepare minutes price data
 
 df_AH = pd.read_excel('data/df_AH.xlsx')
 
+"""港股通的交易费用包含佣金、股票印花税、交易徵费、交易费、中央结算收费。
+1、佣金：每宗交易金额的0.25%，支付予证券公司  
+2、股票印花税：每宗交易金额的0.1%，支付予香港政府  
+3、交易徵费：每宗交易金额的0.003%，支付予证监会  
+4、交易费：每宗交易金额的0.005%，支付予交易所  
+5、中央结算收费：每宗交易金额的0.002%，支付予结算所(最低港币2元,最高港币100元)"""
+
+
 class stock:
     def __init__(self, name, df_AH):
+        self.transaction_rate = np.sum([0.0025, 0.001, 0.00003, 0.00005, 0.00002])
         self.name = name
         self.df_AH = df_AH
+        self.n = len(self.df_AH)
         self.df_AH.columns = ['A','H']
-        self.rolling_window_size = 60
+        self.rolling_window_size = 60 * 24 * 3
         self.df_AH['DR'] =1 - (self.df_AH['H'])/self.df_AH['A']
+        self.adjust_boundry()
         self.df_AH['DR_mean'] = self.df_AH['DR'].rolling(self.rolling_window_size).mean()
-        self.df_AH['DR_std'] = self.df_AH['DR'].rolling(self.rolling_window_size).std()
+        self.df_AH['DR_std'] = self.df_AH['DR_adjust'].rolling(self.rolling_window_size).std()
         self.df_AH['DR_ub'] = self.df_AH['DR_mean']+self.df_AH['DR_std'] * 1
-        self.df_AH['DR_lb'] = self.df_AH['DR_mean']-self.df_AH['DR_std'] * 0.5
+        self.df_AH['DR_lb'] = self.df_AH['DR_mean']-self.df_AH['DR_std'] * 0.25
         
+    
+    def adjust_boundry(self):
+        H = self.df_AH['H'].values
+        for t in range(self.n):
+            if H[t] > self.df_AH['A'][t]:
+                H[t] *= 1 + self.transaction_rate
+            elif H[t] < self.df_AH['A'][t]:
+                H[t] *= 1 - self.transaction_rate
+        self.df_AH['H_adjust'] = H
+        self.df_AH['DR_adjust'] = 1 - self.df_AH['H_adjust']/self.df_AH['A']
+
+
     def draw_price(self, save_path=''):
         plt.plot(self.df_AH.dropna()['A'],linewidth=1,label='A')
         plt.plot(self.df_AH.dropna()['H'],linewidth=1,label='HK')
@@ -117,18 +140,19 @@ class stock:
             elif isBuy:
                 buyPoint[t] = 1
                 isBuy = False
-                psntVolm = psntValue/open[t]
+                psntVolm = psntValue/(open[t] * (1 + self.transaction_rate))
                 isAllIn = True
             else:# sell
                 sellPoint[t] = 1
+                psntValue *= (1 - self.transaction_rate)
                 isSell = False
                 isAllIn = False
         # value, psntValue, buyPoint, sellPoint = trading_rule(df_AH)
 
         plt.plot(value, label='Value',linewidth=1)
         if show_transaction:
-            plt.scatter(np.arange(len(value))[buyPoint == 1],value[buyPoint == 1],color='red',label='Buy',s=1)
-            plt.scatter(np.arange(len(value))[sellPoint == 1],value[sellPoint == 1],color='green',label='Sell',s=1)
+            plt.scatter(np.arange(len(value))[buyPoint == 1],value[buyPoint == 1],color='red',label='Buy',s=10)
+            plt.scatter(np.arange(len(value))[sellPoint == 1],value[sellPoint == 1],color='green',label='Sell',s=10)
         plt.legend()
         plt.title(self.name + '\nFinal Value: {:.2f}'.format(psntValue))
         if save_path != '':
@@ -136,18 +160,33 @@ class stock:
                 os.makedirs(save_path)
             plt.savefig(save_path+ '/' + self.name + 'value.png',dpi=600, bbox_inches='tight')
         plt.show()
+
+        if show_transaction:
+            plt.plot(df['DR'], color='gray', label='DiscountRate',linewidth=0.2,zorder=1)
+            plt.plot(df['DR_mean'], color='orange', label='Mean')
+            plt.plot(df['DR_ub'], color='orange',linestyle='--', label='ub')
+            plt.plot(df['DR_lb'], color='orange',linestyle='--', label='lb')
+            plt.scatter(df.index[buyPoint == 1],df['DR'][buyPoint == 1],color='red',label='Buy',s=10,zorder=2)
+            plt.scatter(df.index[sellPoint == 1],df['DR'][sellPoint == 1],color='green',label='Sell',s=10,zorder=2)
+            plt.legend()
+            plt.title(self.name + '\nDiscount Rate')
+            # if save_path != '':
+            #     if not os.path.exists(save_path):
+            #         os.makedirs(save_path)
+            #     plt.savefig(save_path + '/' + self.name + 'DR_transaction.png',dpi=600, bbox_inches='tight')
+            plt.show()
         # return value, psntValue, buyPoint, sellPoint
 
 stock1 = stock('招商银行',df_AH=df_AH[['招商银行','招商银行.1']])
 stock2 = stock('工商银行',df_AH=df_AH[['工商银行','工商银行.1']])
 stock3 = stock('农业银行',df_AH=df_AH[['农业银行','农业银行.1']])
 
-stock1.draw_price(save_path='data')
-stock1.draw_DR(save_path='data')
-stock1.trading_rule(save_path='data')
-stock2.draw_price(save_path='data')
-stock2.draw_DR(save_path='data')
-stock2.trading_rule(save_path='data')
-stock3.draw_price(save_path='data')
-stock3.draw_DR(save_path='data')
-stock3.trading_rule(save_path='data')
+# stock1.draw_price()
+# stock1.draw_DR()
+stock1.trading_rule(show_transaction=True)
+# stock2.draw_price(save_path='data')
+# stock2.draw_DR(save_path='data')
+# stock2.trading_rule(save_path='data')
+# stock3.draw_price(save_path='data')
+# stock3.draw_DR(save_path='data')
+# stock3.trading_rule(save_path='data')
